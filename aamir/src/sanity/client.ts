76cbce1666bd7c1b12e99c3e06346ab41c7ab4f6
Ldefalco11 <l.defalco14@studenti.unisa.it>
2026-07-22 11:57:55 +0200

    fix: correct SanityImageSource import path for @sanity/image-url v2

--- aamir/src/sanity/client.ts
+++ aamir/src/sanity/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import type { SanityImageSource } from "@sanity/image-url";
 import { apiVersion, dataset, projectId } from "./env";
 
 export const client = createClient({
